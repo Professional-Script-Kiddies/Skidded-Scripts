1c6ff2d2a71a84bbe1d1996f2fae0e27432769a2
XJMI <59071906+XJMI@users.noreply.github.com>
2022-04-16 19:37:26 -0400

    Update NetworkDestroy.lua

--- NetworkDestroy.lua
+++ NetworkDestroy.lua
@@ -1,10 +1,22 @@
-local Target = "PlayerName"
-
+local TARGET = "PlayerName"
+    
 game:GetService('RunService').Heartbeat:connect(function()
-   for i, v in pairs(workspace[Target]:GetDescendants()) do
-           if v:IsA("BasePart") or v:IsA("Part") then
+    for i, v in pairs(workspace[TARGET]:GetDescendants()) do
+        if v:IsA("BasePart") or v:IsA("Part") then
             sethiddenproperty(v, "NetworkIsSleeping", true)
-            sethiddenproperty(v, "NetworkOwnership", false)
         end
-   end
+    end
 end)
+    
+print(pcall(function()
+    for i,v in pairs(game.Players[TARGET].Character:GetChildren()) do
+        if v:IsA("Tool") and v:FindFirstChildOfClass("Part"):FindFirstChildOfClass("Sound") then
+            coroutine.wrap(function()
+                while true do
+                    wait()
+                    v:FindFirstChildOfClass("Part"):FindFirstChildOfClass("Sound").TimePosition = math.random(0, 100)
+                    end
+                end)()
+            end
+        end
+    end))
