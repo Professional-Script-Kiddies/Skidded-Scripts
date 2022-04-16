8eb1cbf98ca6d30145b7c73c8687c759a13a083a
XJMI <59071906+XJMI@users.noreply.github.com>
2022-04-16 19:32:11 -0400

    rewrite

--- FastDupe.lua
+++ FastDupe.lua
@@ -1,30 +1,15 @@
-for i=1,tonumber(9) or 1 do
+for i = 1, tonumber(10) or 1 do
 
-local SelectDance = 'dance2' -- Modes: dance1, dance2, dance3
-local tp = CFrame.new(0, 99999999, 0)
+local dances = { "dance1", "dance2", "dance3" }
+local randomize = math.random(1, #dances)
+local SelectDance = dances[randomize]
 
-if game.PlaceId == 6142500718 then
-         -- // Radio Testing Game \\ --
-        tp = CFrame.new(529, 311, 513)
-    else
-if game.PlaceId == 417267366 then
-         -- // Dollhouse Roleplay \\ --
-        tp = CFrame.new(5534, 50, -17091) -- Middle Sandbox
---      tp = CFrame.new(5554, 44, -17083) -- Tree Top
---      tp = CFrame.new(5502, 95, -17107) -- Top Of DullHause
---      tp = CFrame.new(5539, 39, -17137) -- On Top Of Sign
-        else
-            print("UNKNOWN GAME: "..game.PlaceId" ")
-            tp = CFrame.new(0, 99999999, 0)
-    end
-end
-  
 		game:GetService'Players':Chat(("/e "..SelectDance))
 		char = game.Players.LocalPlayer.Character
 		game.Players.LocalPlayer.Character = nil
 		game.Players.LocalPlayer.Character = char
 		char.Animate:Destroy()
-		char.HumanoidRootPart.CFrame = tp
+		char.HumanoidRootPart.CFrame = CFrame.new(5534, 50, -17091)
 		wait(0.10) -- .1
 		char.HumanoidRootPart.Anchored = true
 		
@@ -41,6 +26,7 @@ end
 			v.Parent = char
 			v.Parent = workspace
 			t[i]= v
+			game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):UnequipTools()
 		end
 		
 		char.Humanoid:Destroy()
