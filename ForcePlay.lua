424533a725d3e81702eeb8a391f52af3ceda8fa1
XJMI <59071906+XJMI@users.noreply.github.com>
2022-04-22 00:08:53 -0400

    stronger loop

--- ForcePlay.lua
+++ ForcePlay.lua
@@ -1,12 +1,10 @@
-print(pcall(function()
-    for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+for i, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
         if v:IsA("Tool") and v:FindFirstChildOfClass("Part"):FindFirstChildOfClass("Sound") then
             coroutine.wrap(function()
-                while true do
-                    wait()
+                game:GetService("RunService").Heartbeat:Connect(function()
                     v:FindFirstChildOfClass("Part"):FindFirstChildOfClass("Sound").Playing = true
-                end
+                end)
             end)()
         end
-    end
-end))
+end
+
