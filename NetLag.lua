7f4c24b4fc4246499161eafdcd6d9757ce22e16c
XJMI <59071906+XJMI@users.noreply.github.com>
2022-05-08 18:47:13 -0400

    Update NetLag.lua

--- NetLag.lua
+++ NetLag.lua
@@ -7,16 +7,3 @@ game:GetService('RunService').Heartbeat:connect(function()
         end
     end
 end)
-    
-print(pcall(function()
-    for i,v in pairs(game.Players[TARGET].Character:GetChildren()) do
-        if v:IsA("Tool") and v:FindFirstChildOfClass("Part"):FindFirstChildOfClass("Sound") then
-            coroutine.wrap(function()
-                while true do
-                    wait()
-                    v:FindFirstChildOfClass("Part"):FindFirstChildOfClass("Sound").TimePosition = math.random(0, 100)
-                    end
-                end)()
-            end
-        end
-    end))
